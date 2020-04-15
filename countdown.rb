5226a9f2929cb1e1e2c07bf9da7a9299abacc99d
Devone Austin <devone.austin@gmail.com>
2020-04-15 01:03:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -25,7 +25,14 @@ class Integer
    end
 end
 
-def countdown_with_sleep(countdown)
+def countdown_with_sleep(number)
   time = Time.now
-  sleep 1.seconds until Time.now > time + 10.seconds
+  sleep 1.seconds until Time.now >= time + 10.seconds
+  number = 10
+  while number < 12
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    break if number == 0
+  end
+  "HAPPY NEW YEAR!"
 end
